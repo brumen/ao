478c9ce7972a893754cf6eddd89b6572106e1fe2
brumen <gorazd.brumen@gmail.com>
2018-02-08 01:59:53 -0500

    Major improvements.

--- ao_scripts/recompute_option.py
+++ ao_scripts/recompute_option.py
@@ -1,40 +1,17 @@
 # re-computes the option from the forwards given
-
+import logging
 import numpy as np
 import json
-import time 
-from flask import jsonify
 
 # ao local modules
-import ao_codes
-import air_option as ao
-from get_data import get_data_dict
+from air_option import compute_option_val
+from get_data   import get_data_dict
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 
-def print_for_js( valid_inp
-                , return_ind
-                , output
-                , price_range
-                , flights_v
-                , reorg_flights_v
-                , minmax_v
-                , do_nothing = False ):
-    """
-    reorganizes the data so that JavaScript can reorganize them better
-
-    """
-
-    return jsonify({'valid_inp'    : valid_inp,
-                    'return_ind'   : return_ind,
-                    'price'        : output,
-                    'flights'      : flights_v,
-                    'reorg_flights': reorg_flights_v,
-                    'minmax'       : minmax_v,
-                    'price_range'  : price_range,
-                    'do_nothing'   : do_nothing})
-
-
-# TODO: FIX THIS PART HERE.
 def recompute_option(form):
     """
 
@@ -57,86 +34,49 @@ def recompute_option(form):
     # flights to select
     sel_flights = form['flights_selected']  # in string form, convert to dict
     sel_flights_dict = json.loads(sel_flights)  # in dict form
-    lt = time.localtime()
-    as_of = str(lt.tm_year) + '_' + str(lt.tm_mon) + '_' + str(lt.tm_mday) + '_' + \
-            str(lt.tm_hour) + '_' + str(lt.tm_min) + '_' + str(lt.tm_sec)
-    inquiry_dir = ao_codes.inquiry_dir + 'inquiry_solo/'
-    inquiry_file = inquiry_dir + 'inquiry_' + as_of + '_from_recompute.inq'
-
-    if is_one_way:
-        # these 4 below are not important, so they are hardcoded
-        option_ret_start_date = '20161212'
-        option_ret_end_date   = '20161213'
-        inbound_date_start    = '2016-12-20'
-        inbound_date_end      = '2016-12-21'
 
     if not all_valid:
-        print_for_js(False, False, '-1', {}, {}, {}, {}, True)
+        return False, {}
 
     else:
-        # with suppress_stdout():
-        if is_one_way:
-            result, price_range, flights_v, reorg_flights_v, minmax_v = \
-                    ao.compute_option_val(origin_place        = origin_place,
-                                          dest_place          = dest_place,
-                                          flights_include     = sel_flights_dict,
-                                          option_start_date   = option_start,
-                                          option_end_date     = option_end,
-                                          outbound_date_start = outbound_start,
-                                          outbound_date_end   = outbound_end,
-                                          carrier             = carrier_used,
-                                          cabinclass          = cabin_class,
-                                          adults              = np.int(nb_people),
-                                          K                   = np.double(strike),
-                                          recompute_ind       = True )
-        else:
-            result, price_range, flights_v, reorg_flights_v, minmax_v = \
-                    ao.compute_option_val(origin_place          = origin_place,
-                                          dest_place            = dest_place,
-                                          flights_include       = sel_flights_dict,
-                                          option_start_date     = option_start,
-                                          option_end_date       = option_end,
-                                          outbound_date_start   = outbound_start,
-                                          outbound_date_end     = outbound_end,
-                                          option_ret_start_date = option_start_ret,
-                                          option_ret_end_date   = option_end_ret,
-                                          inbound_date_start    = inbound_start,
-                                          inbound_date_end      = inbound_end,
-                                          carrier               = carrier_used,
-                                          cabinclass            = cabin_class,
-                                          adults                = np.int(nb_people),
-                                          K                     = np.double(strike),
-                                          return_flight         = True,
-                                          recompute_ind         = True )
+
+        way_args = { 'origin_place': origin_place
+                   , 'dest_place':   dest_place
+                   , 'flights_include': sel_flights_dict
+                   , 'option_start_date': option_start
+                   , 'option_end_date': option_end
+                   , 'outbound_date_start': outbound_start
+                   , 'outbound_date_end': outbound_end
+                   , 'carrier': carrier_used
+                   , 'cabinclass': cabin_class
+                   , 'adults' :np.int(nb_people)
+                   , 'K':  np.double(strike)
+                   , 'recompute_ind': True }
+
+        if not is_one_way:
+            way_args.update({ 'option_ret_start_date': option_start_ret
+                            , 'option_ret_end_date': option_end_ret
+                            , 'inbound_date_start': inbound_start
+                            , 'inbound_date_end': inbound_end
+                            , 'return_flight': True})
+
+        # do the recomputation
+        result, price_range, flights_v, reorg_flights_v, minmax_v = \
+            compute_option_val(way_args)
 
         if result == 'Invalid':
-            print_for_js(False, False, '-1', [], [], [], [], True)  # invalid entries
+            logger.info('Invalid results of recomputation')
+
+            return False, {}  # invalid entries
 
         else:
-            # THIS IS WRONG
-            # result_ref = str(np.int(result['avg'])) - DOESNT WORK, NOT IMPORTANT
-            result_ref = str(np.int(1.))
+            logger.info('Recomputation finished')
 
-            print_for_js(True, return_ow, result_ref, price_range,
-                         flights_v, reorg_flights_v, minmax_v, False)
-            # write an inquiry in the inquiry log
-            res_write = {'origin_place': origin_place,
-                         'dest_place': dest_place,
-                         'flights_include': sel_flights_dict,
-                         'option_start_date': option_start,
-                         'option_end_date': option_end,
-                         'outbound_date_start': outbound_start,
-                         'outbound_date_end': outbound_end,
-                         'carrier': carrier_used,
-                         'cabinclass': cabin_class,
-                         'adults': nb_people,
-                         'K': np.double(strike)}
-
-            if not is_one_way:
-                res_write['option_ret_start_date'] =  option_start_ret
-                res_write['option_ret_end_date'] = option_end_ret
-                res_write['inbound_date_start'] = inbound_start
-                res_write['inbound_date_end'] = inbound_end
-
-            with open(inquiry_file, 'w') as fo:
-                fo.write(json.dumps(res_write))
+            return True, {'valid_inp': True,
+                          'return_ind': not is_one_way,
+                          'price': result,
+                          'flights': flights_v,
+                          'reorg_flights': reorg_flights_v,
+                          'minmax': minmax_v,
+                          'price_range': price_range,
+                          'do_nothing': True }
