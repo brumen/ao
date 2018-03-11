d954b61c70e0c5c0f17eff336b468b9622b2765e
brumen <gorazd.brumen@gmail.com>
2018-03-10 19:35:09 -0500

    Major improvements.

--- ao_scripts/verify_origin.py
+++ ao_scripts/verify_origin.py
@@ -1,5 +1,7 @@
 # Module for checking that the origin is valid
 
+from typing import List
+
 from ao_codes import iata_cities_codes   as IATA_CITIES_CODES,\
                      iata_codes_cities   as IATA_CODES_CITIES,\
                      iata_airlines_codes as IATA_AIRLINES_CODES,\
@@ -23,8 +25,8 @@ def is_valid_origin(origin : str) -> bool :
     if origin is None:
         return False
 
-    ret_cand_1 = filter( lambda x: x.startswith(origin.upper())
-                       , IATA_CITIES_CODES_l + IATA_CODES_CITIES_l )
+    ret_cand_1 = list( filter( lambda x: x.startswith(origin.upper())
+                             , IATA_CITIES_CODES_l + IATA_CODES_CITIES_l ) )
 
     return not (len(ret_cand_1) == 0)
 
@@ -41,6 +43,47 @@ def is_valid_airline(airline : str) -> bool :
     if airline is None:
         return False
 
-    ret_cand_1 = filter( lambda x: x.upper().startswith(airline.upper())
-                       , IATA_AIRLINES_CODES_l + IATA_CODES_AIRLINES_l )
+    ret_cand_1 = list( filter( lambda x: x.upper().startswith(airline.upper())
+                             , IATA_AIRLINES_CODES_l + IATA_CODES_AIRLINES_l ) )
     return not (len(ret_cand_1) == 0)
+
+
+def show_airport_l(airport_partial_name: str) -> tuple:
+    """
+    Returns the list of airports from term.
+
+    :param airport_partial_name: Partial name of the airport one is searching for.
+    :returns: the list of potential candidates for the airport
+    """
+
+    ret_cand_1 = list(filter( lambda x: airport_partial_name.upper() in x
+                            , IATA_CITIES_CODES_l + IATA_CODES_CITIES_l ) )
+
+    if not ret_cand_1:  # nothing found, report problem
+        return []             , False
+
+    elif len(ret_cand_1) < 10:
+        return ret_cand_1     , True
+
+    else:
+        return ret_cand_1[:10], True
+
+
+def show_airline_l(airline_partial_name : str) -> tuple:
+    """
+    Returns the list of IATA airlines for the partial name searched.
+
+    :param airline_partial_name: partial name of the airline one is searching for.
+    :returns: list of names one for the airlines one is searching for.
+    """
+
+    ret_cand_1 = list(filter( lambda x: airline_partial_name.upper() in x.upper()
+                            , IATA_AIRLINES_CODES_l) )
+
+    if not ret_cand_1:  # nothing found
+        return []             , False
+
+    if len(ret_cand_1) < 10:
+        return ret_cand_1     , True
+    else:
+        return ret_cand_1[:10], True
