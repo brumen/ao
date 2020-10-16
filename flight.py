b8e9499c1fb9905730fc3b8cf3a4db777c5c79f0
brumen <gorazd.brumen@gmail.com>
2020-10-16 10:16:12 -0400

    adding sql alchemy of dealing w/ fliights.

---
+++ flight.py
@@ -0,0 +1,74 @@
+# flight class for ORM, trade access
+
+from sqlalchemy                 import Column, Integer, String, DateTime, ForeignKey, BigInteger, Table
+from sqlalchemy.orm             import relation
+from sqlalchemy.ext.declarative import declarative_base
+
+AOORM = declarative_base()  # common base class
+
+
+class Flight(AOORM):
+    """ Description of the flight.
+    """
+
+    __tablename__ = 'flight_ids'
+
+    flight_id      = Column(Integer, primary_key=True)
+    flight_id_long = Column(String)
+    orig           = Column(String)
+    dest           = Column(String)
+    dep_date       = Column(DateTime)
+    arr_date       = Column(DateTime)
+    carrier        = Column(String)
+
+
+t_trades_flights = Table( 'trades_flights'
+                        , AOORM.metadata
+                        , Column('entry_id' , BigInteger, primary_key=True)
+                        , Column('trade_id' , Integer   , ForeignKey('option_positions.position_id'))
+                        , Column('flight_id', Integer   , ForeignKey('flight_ids.flight_id')        )
+                        , )
+
+
+class AOTrade(AOORM):
+    """ AirOptions trade.
+    """
+
+    __tablename__ = 'option_positions'
+
+    position_id           = Column(Integer, primary_key=True)
+    origin                = Column(String)
+    dest                  = Column(String)
+    option_start_date     = Column(DateTime)
+    option_end_date       = Column(DateTime)
+    option_ret_start_date = Column(DateTime)
+    option_ret_end_date   = Column(DateTime)
+    outbound_date_start   = Column(DateTime)
+    outbound_date_end     = Column(DateTime)
+    inbound_date_start    = Column(DateTime)
+    inbound_date_end      = Column(DateTime)
+    strike                = Column(DateTime)
+    carrier               = Column(String)
+    nb_adults             = Column(Integer)
+    cabinclass            = Column(String)
+
+    flights = relation('Flight', secondary=t_trades_flights)
+
+
+# example of using flights.
+# from sqlalchemy.orm import sessionmaker, create_engine
+# engine = create_engine('mysql://brumen@localhost/ao')
+# Session = sessionmaker(bind=engine)
+# sess = Session()
+# res1 = sess.query(Flight)
+# tr1 = sess.query(AOTrade).filter_by(position_id=2).all()[0]
+# tr2 = tr1.flights
+# print(tr2)
+
+# typical query which implements AOTrade:
+# select fid.flight_id_long, fo.price
+# from flight_ids fid, option_positions op, trades_flights tf, flights_ord fo
+# where op.origin = 'SFO' and op.dest = 'EWR'
+#      and op.position_id = tf.trade_id
+#      and tf.flight_id = fid.flight_id
+#      and fo.flight_id = tf.flight_id
