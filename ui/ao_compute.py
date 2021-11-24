a1226b4cfc360e59774bd6811a62b898e36029a2
brumen <gorazd.brumen@gmail.com>
2021-11-24 14:57:32 -0500

    fixing the UI.

--- ui/ao_compute.py
+++ ui/ao_compute.py
@@ -9,7 +9,11 @@ import logging
 import sys
 sys.path.append('/home/brumen/work/')
 
-from ao.air_option_derive import AirOptionsFlightsExplicitSky as AOS
+from typing import Optional
+
+from ao.air_option_derive     import AirOptionsFlightsExplicitSky as AOS
+from ao.ui.autocomplete_combo import AutocompleteCombobox
+from ao.iata.codes            import get_airline_code, get_city_code
 
 # example:
 #ao = AOS(datetime.date(2016, 9, 25), origin='SFO', dest='EWR', outbound_date_start=datetime.date(2016, 10, 1), outbound_date_end=datetime.date(2016, 10, 4), K = 100., carrier='UA')
@@ -23,30 +27,59 @@ logger_handler.setLevel(logging.INFO)
 logger.addHandler(logger_handler)
 
 
+def entry_field(text_ : str, row : int, default_val : Optional[str] = None):
+    """ Creates the tk.Entry component and a label component in a row row, with default value.
 
-def entry_field(text_, row, default_val = None):
+    :param text_: text to be displayed in the label.
+    :param row: which row do the two elements belong.
+    :param default_val: default value of the Entry field.
+    :returns: Entry component of the two created.
+    """
+
     tk.Label(top, text=text_).grid(row=row, column=0)
 
     if default_val is None:
-        e1 = tk.Entry(top, bd=3)
+        entry_comp = tk.Entry(top, bd=3)
     else:
         d_val = tk.StringVar()
-        e1 = tk.Entry(top, bd=3, textvariable=d_val)
+        entry_comp = tk.Entry(top, bd=3, textvariable=d_val)
         d_val.set(default_val)
 
-    e1.grid(row=row, column=1)
+    entry_comp.grid(row=row, column=1)
 
-    return e1
+    return entry_comp
 
 
+def combo_field(text_, row, default_val = None, completion_list = None):
+    """ Same as entry_comp, but w. AutocompleteCombobox component.
+    """
+
+    tk.Label(top, text=text_).grid(row=row, column=0)
+
+    if default_val is None:
+        combo_comp = AutocompleteCombobox(top)
+    else:
+        d_val = tk.StringVar()
+        combo_comp = AutocompleteCombobox(top, textvariable=d_val)
+        combo_comp.set_completion_list([] if completion_list is None else completion_list)
+        d_val.set(default_val)
+
+    combo_comp.grid(row=row, column=1)
+
+    return combo_comp
+
+
+# main file.
 top = tk.Tk()
 
+airports = get_city_code('')
+airlines = get_airline_code('')
 # origin
 market_date    = entry_field('market date'   , 0, '2016-9-15')
-origin         = entry_field('origin'        , 1, 'SFO')
-dest           = entry_field('dest'          , 2, 'EWR')
+origin         = combo_field('origin'        , 1, default_val='SFO', completion_list=airports)
+dest           = combo_field('dest'          , 2, default_val='EWR', completion_list=airports)
 strike         = entry_field('strike'        , 3, '100')
-carrier        = entry_field('carrier'       , 4, 'UA')
+carrier        = combo_field('carrier'       , 4, default_val='UA', completion_list=airlines)
 outbound_start = entry_field('outbound start', 5, '2016-10-1')
 outbound_end   = entry_field('outbound end'  , 6, '2016-10-4')
 
@@ -65,9 +98,8 @@ compute_button = tk.Button(top
 
 
 def compute_ao(market_date : str, origin : str, dest : str, strike : str, carrier : str, outbound_start : str, outbound_end : str):
-    ''' Handles the errors of the conversion, and output.
-
-    '''
+    """ Handles the errors of the conversion, and output.
+    """
 
     try:
         md = datetime.datetime.strptime(market_date, '%Y-%m-%d').date()
@@ -89,12 +121,13 @@ def compute_ao(market_date : str, origin : str, dest : str, strike : str, carrie
         oe = datetime.datetime.strptime(outbound_end, '%Y-%m-%d').date()
     except Exception as e:
         logger.error(f'Outbound end date {outbound_end} could not be converted to date: {e}, {type(e)}')
+        return None
 
     try:
         st = float(strike)
     except Exception as e:
         logger.error(f'Strike {strike} could not be converted to float: {e}, {type(e)}')
-
+        return None
 
     aos = AOS( md
                 , origin = origin
@@ -104,7 +137,6 @@ def compute_ao(market_date : str, origin : str, dest : str, strike : str, carrie
                 , K = st
                 , carrier = carrier).PV()
 
-    print(aos)
     res_var.set(str(aos))
 
 
